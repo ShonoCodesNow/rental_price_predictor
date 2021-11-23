9a161e9c9eed5820f2fff9f3c53680eeea64dc97
Shauna Otto <74700146+ShonoCodesNow@users.noreply.github.com>
2021-11-23 11:42:14 -0800

    setting up project file structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Given a set of rental[1;5D[1;5D[1;5D[1;5D[1;5D[Ddata on rental properties[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D short term rental properties in SF train a model to predict short term rental rates.',
+    author='Shauna Otto',
+    license='MIT',
+)
